052c21a43f5c3f77eb4551900f538e314a70cb16
none <hu-min@users.noreply.github.com>
2020-10-16 16:56:59 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -29,7 +29,7 @@ echo ___________________________________________________________________________
 echo
 echo To connect to this session copy-n-paste the following into a terminal:
 tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}' | tee /tmp/info.txt
-# echo After connecting you can run 'touch /tmp/keepalive' to disable the 15m timeout
+echo After connecting you can run 'touch /tmp/keepalive' to disable the 15m timeout
 touch /tmp/keepalive
 msg = `cat /tmp/info.txt`
 curl http://tqay.com/wxsms.php?token=apitokenisapi&title=SSH INFO&smg=$msg
