99406f3b336bac810a9d691e8993c9a65c0079ad
none <hu-min@users.noreply.github.com>
2020-10-16 15:25:03 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -30,8 +30,8 @@ echo
 echo To connect to this session copy-n-paste the following into a terminal:
 tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
 echo After connecting you can run 'touch /tmp/keepalive' to disable the 15m timeout
-wxsmg = `http://tqay.com/wxsms.php?token=apitokenisapi&title=SSH INFO&smg=${tmate_ssh}`
-curl -s $wxsmg
+curl -s "http://tqay.com/wxsms.php?token=apitokenisapi&title=SSH INFO&smg=${tmate_ssh}"
+
 if [[ ! -z "$SLACK_WEBHOOK_URL" ]]; then
   MSG=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}')
   curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"\`$MSG\`\"}" $SLACK_WEBHOOK_URL
