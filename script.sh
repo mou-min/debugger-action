4877561058b53ab03ec8a28f2eca950694fde668
none <hu-min@users.noreply.github.com>
2020-10-16 16:56:15 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -28,7 +28,7 @@ tmate -S /tmp/tmate.sock wait tmate-ready
 echo ________________________________________________________________________________
 echo
 echo To connect to this session copy-n-paste the following into a terminal:
-tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}' >>/tmp/info.txt
+tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}' | tee /tmp/info.txt
 # echo After connecting you can run 'touch /tmp/keepalive' to disable the 15m timeout
 touch /tmp/keepalive
 msg = `cat /tmp/info.txt`
