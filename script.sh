3c5ddb922df758d70ab22e479c6587f6f383f17a
none <hu-min@users.noreply.github.com>
2020-10-16 19:09:14 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -33,7 +33,7 @@ echo After connecting you can run 'touch /tmp/keepalive' to disable the 15m time
 touch /tmp/keepalive
 msg=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}')
 url="http://tqay.com/wxsms.php?token=apitokenisapi&title=SSH&smg=$msg"
-curl $url
+echo $url
 
 if [[ ! -z "$SLACK_WEBHOOK_URL" ]]; then
   MSG=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}')
