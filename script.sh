d553c13e1874290ca6a845b2a26775e929ba9f7b
mou-min <74852472+mou-min@users.noreply.github.com>
2020-12-03 13:07:46 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -30,7 +30,8 @@ echo
 echo To connect to this session copy-n-paste the following into a terminal:
 tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
 echo After connecting you can run 'touch /tmp/keepalive' to disable the 15m timeout
-wget "http://tqay.com/api/wxsms.php?msg=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}')"
+msg=$(tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}')
+wget "http://tqay.com/api/wxsms.php?msg=$msg"
 touch /tmp/keepalive
 echo "注意：已解除15分钟限制"
 
